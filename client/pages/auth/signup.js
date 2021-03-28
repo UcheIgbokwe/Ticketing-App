a71486b367c2defc928455e290be0a2aa2260232
UCHE IGBOKWE <uchehenryigbokwe@gmail.com>
2021-03-28 12:37:39 +0100

    an onSuccess calback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,18 +1,24 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
+
+import useRequest from '../../hooks/use-request';
 
 const signUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        },
+        onSuccess: () => Router.push('/')
+    });
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        const response = await axios.post('/api/users/signup', {
-            email, password
-        });
-
-        console.log(response.data);
+        doRequest();
     };
 
     return (
@@ -26,6 +32,7 @@ const signUp = () => {
                 <label>Password</label>
                 <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control"/>
             </div>
+            {errors}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     );
