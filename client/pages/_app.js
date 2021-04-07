be21cfad18507291a7272c89a720d5353d50d2e2
Uche Igbokwe <uchehenryigbokwe@gmail.com>
2021-04-07 14:24:52 +0100

    signout

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
+
 import buildClient from '../api/build-client';
+import Header from '../components/header';
 
 
 
-const globalCss = ({ Component, pageProps }) => {
+const globalCss = ({ Component, pageProps, currentUser }) => {
     return <div>
-        <h1>Header!</h1>
+        <Header currentUser={currentUser}/>
         <Component {...pageProps} />
     </div>
 };
@@ -14,9 +16,17 @@ const globalCss = ({ Component, pageProps }) => {
     const client = buildClient(appContext.ctx);
     const { data } = await client.get('/api/users/currentuser');
 
-    //console.log(data);
-    
-    return data;
+    let pageProps = {};
+    if(appContext.Component.getInitialProps)
+    {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+
+
+    return {
+        pageProps,
+        ...data
+    }
  };
 
 export default globalCss;
