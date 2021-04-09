2b7bab444368eca34bf8fd9274b1b18ee0f0444a
Uche Igbokwe <uchehenryigbokwe@gmail.com>
2021-04-09 11:02:02 +0100

    deployment file update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,8 +7,11 @@ const start = async () => {
     if(!process.env.JWT_KEY){
         throw new Error('JWT KEY not defined');
     }
+    if(!process.env.MONGO_URI){
+        throw new Error('MONGO_URI not defined');
+    }
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
